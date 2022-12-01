87e2521d6268319492e55a873826aad5aa28b1f3
Manjunath Singh  Singh <>
2022-12-01 05:13:39 +0000

    hii

---
+++ set01/problem01.c
@@ -0,0 +1,86 @@
+/*#include<stdio.h>
+main()
+{
+int arr[5],i,sum=0;
+printf("Enter the 5 elements");
+for(i=0;i<=4;i++)
+{
+scanf(%d,&arr[i]);
+}
+for(i=0;i<=4;i++)
+{
+sum=sum+arr[i];
+}
+printf("%d",sum);
+int avg=sum/5;
+printf("\n%d",avg);
+}*/
+/*#include <stdio.h>
+  
+int main()
+{
+    // print name
+    printf("Name : GeeksforGeeks");
+    return 0;
+}
+#include <stdio.h>
+  
+int main()
+{
+    char name[20];
+    printf("Enter name: ");
+  
+    // user input will be taken here
+    scanf("%s", name);
+    printf("Your name is %s.", name);
+    return 0;
+}
+
+#include <stdio.h> 
+ int main()  
+  {
+     printf("Name   : Alexandra Abramov\n"); 
+     printf("DOB    : July 14, 1975\n"); 
+     printf("Mobile : 99-9999999999\n"); 
+     //return(0); 
+  }
+#include<stdio.h>
+int main()
+{
+ printf("Name"); 
+}*/
+
+/*#include <stdio.h>
+int main()
+{
+    char name[20];
+    printf("Enter name: ");
+  
+    // user input will be taken here
+    scanf("%s", name);
+    printf("Your name is %s.", name);
+    //return 0;
+}*/
+/*#include "stdio.h"
+
+void Greeting (char nameInput[100]) {
+	printf("Hello %s! Welcome to the world of FUNCTIONS!",nameInput);
+}
+
+
+int main(void) {
+	
+	char name[100];
+  printf("What is your name?");
+  scanf("%s",&name);
+  
+  Greeting(name);
+  
+  return 0;*/
+
+#include <stdio.h>
+int main();
+{
+  printf("Manjunath");
+  return 0;
+}
---
+++ set01/problem02.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+  innt a,b,sum;
+  printf("Enter two numbers");
+  scaf(%d,&a,&b);
+  printf("Sum");
+  sum=a+b;
+}
